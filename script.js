8076df577e48692336158c077d75b5fdfe73adc2
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-20 22:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let modalDiv = document.getElementsByClassName("modal")[0];
+let closeModal = document.getElementsByClassName("close-modal")[0];
+let modalBtn = document.getElementById("openModal");
+
+modalBtn.addEventListener("click", () => {
+    modalDiv.style.display = "block";
+});
+closeModal.addEventListener("click",()=>{
+	modalDiv.style.display = "none";
+})
+
+
