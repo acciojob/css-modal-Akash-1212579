aa5fd2b6f0da8f65f2854d054d69fcc9a064170b
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-20 22:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 let modalDiv = document.getElementsByClassName("modal")[0];
 let closeModal = document.getElementsByClassName("close-modal")[0];
 let modalBtn = document.getElementById("openModal");
-
+let header = document.getElementsByClassName("modal-header")[0];
 modalBtn.addEventListener("click", () => {
     modalDiv.style.display = "block";
 });
 closeModal.addEventListener("click",()=>{
 	modalDiv.style.display = "none";
-})
+});
+header.addEventListener("click",()=>{
+	modalDiv.style.display = "none";
+});
+
 
 
